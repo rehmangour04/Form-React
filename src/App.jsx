9e0575f2bec5a1b0c398ba2342866bfbb4671bf6
Ataur Rehman <rehmangour04@gmail.com>
2024-03-25 16:44:53 +0530

    new adding features

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
   const [errorLastName, setErrorLastName] = useState("");
   const [formSubmitted, setFormSubmitted] = useState(false); // Track form submission status
 
-  // Function to handle first name change
   const handleFirstNameChange = (event) => {
     const value = event.target.value;
     setFirstName(value);
@@ -19,7 +18,6 @@ function App() {
     setFormSubmitted(false); // Reset form submission status on input change
   };
 
-  // Function to handle last name change
   const handleLastNameChange = (event) => {
     const value = event.target.value;
     setLastName(value);
@@ -27,7 +25,6 @@ function App() {
     setFormSubmitted(false); // Reset form submission status on input change
   };
 
-  // Function to handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
     if (firstName.trim() === "" || lastName.trim() === "") {
@@ -37,22 +34,16 @@ function App() {
       setErrorLastName(
         lastName.trim() === "" ? "Please fill out this field." : ""
       );
-      setFormSubmitted(false); // Reset form submission status if form is invalid
     } else {
       setFullName(`${firstName} ${lastName}`);
       setErrorFirstName("");
       setErrorLastName("");
-      setFormSubmitted(true); // Set form submission status to true
+      setFormSubmitted(true);
     }
   };
 
   return (
     <form onSubmit={handleSubmit} className="form-container">
-      {/* Initial rendering of "Full Name Display" */}
-      <div className="full-name-container">
-        <label className="full-name-label">Full Name Display:</label>
-        <span className="full-name"> </span>
-      </div>
       <div className="form-group">
         <label htmlFor="firstName">First Name:</label>
         <input
@@ -79,16 +70,17 @@ function App() {
           <span className="error-message">{errorLastName}</span>
         )}
       </div>
-      <button type="submit" className="submit-btn">
+      <button
+        type="submit"
+        className="submit-btn"
+        disabled={!firstName || !lastName}
+      >
         Submit
       </button>
-      {/* Display full name upon form submission */}
-      {formSubmitted && fullName && (
-        <div className="full-name-container">
-          <label className="full-name-label">Full Name:</label>
-          <span className="full-name">{fullName}</span>
-        </div>
-      )}
+      <div className="full-name-container">
+        <label className="full-name-label">Full Name Display:</label>
+        <span className="full-name">{fullName}</span>
+      </div>
     </form>
   );
 }
